482c7eb0abcfc8f9f4588558dd783a7de7ecc3a1
Aleksandra Pawlak <synowa8@gmail.com>
2020-04-28 15:28:25 +0200

    Calc Engine with Initializers and Constructors - 2.0

--- src/com/synowa/calculator/Main.java
+++ src/com/synowa/calculator/Main.java
@@ -10,10 +10,11 @@ public class Main {
 
         MathEquation[] equations = new MathEquation[4];
         //equations[0] = new MathEquation();
-        equations[0] = new MathEquation('d', 100.0d, 50.0d);
-        equations[1] = new MathEquation('a', 25.0d, 92.0d);
-        equations[2] = new MathEquation('s', 225.0d, 17.0d);
-        equations[3] = new MathEquation('m', 11.0d, 3.0d);
+
+        equations[0] = new MathEquation('d',100.0d, 50.0d);
+        equations[1] = new MathEquation('a',25.0d, 92.0d);
+        equations[2] = new MathEquation('s',225.0d, 17.0d);
+        equations[3] = new MathEquation('m',11.0d, 3.0d);
 
         for (MathEquation equation : equations) {
             equation.execute();
@@ -22,13 +23,13 @@ public class Main {
         }
     }
 
-    public static MathEquation create (double leftVal, double rightVal, char opCode){
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-
-        return equation;
-     }
+//    public static MathEquation create (double leftVal, double rightVal, char opCode){
+//        MathEquation equation = new MathEquation();
+//        equation.setLeftVal(leftVal);
+//        equation.setRightVal(rightVal);
+//        equation.setOpCode(opCode);
+//
+//        return equation;
+//     }
 }
 
