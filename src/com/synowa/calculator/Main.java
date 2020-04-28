bb4efabf664ac3f791d906bcf5fe0f1255facab7
Aleksandra Pawlak <synowa8@gmail.com>
2020-04-28 15:22:43 +0200

    Calc Engine with Initializers and Constructors - 1.0

--- src/com/synowa/calculator/Main.java
+++ src/com/synowa/calculator/Main.java
@@ -3,12 +3,17 @@ package com.synowa.calculator;
 public class Main {
 
     public static void main(String[] args) {
+//        MathEquation testEquation = new MathEquation();
+//        testEquation.execute();
+//        System.out.print("test=");
+//        System.out.println(testEquation.getResult());
+
         MathEquation[] equations = new MathEquation[4];
         //equations[0] = new MathEquation();
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for (MathEquation equation : equations) {
             equation.execute();
@@ -24,8 +29,6 @@ public class Main {
         equation.setOpCode(opCode);
 
         return equation;
-
-
-    }
+     }
 }
 
--- src/com/synowa/calculator/MathEquation.java
+++ src/com/synowa/calculator/MathEquation.java
@@ -3,17 +3,49 @@ package com.synowa.calculator;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
-    public double getLeftVal() {return leftVal;}
-    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
-    public double getRightVal() {return rightVal;}
-    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
-    public char getOpCode() {return opCode;}
-    public void setOpCode(char opCode) {this.opCode = opCode;}
+    public double getLeftVal() {
+        return leftVal;
+    }
 
-    public double getResult() { return result;}
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+
+    public double getResult() {
+        return result;
+    }
+
+    public MathEquation() {}
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
     public void execute() {
         switch (opCode) {
