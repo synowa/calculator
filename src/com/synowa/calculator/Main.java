b46ee0c17c0d49fefebb6c8bd0b18256eb76ca12
Aleksandra Pawlak <synowa8@gmail.com>
2020-04-29 17:16:25 +0200

    Calc Engine with More Data Type Capabilities.

--- src/com/synowa/calculator/Main.java
+++ src/com/synowa/calculator/Main.java
@@ -3,14 +3,26 @@ package com.synowa.calculator;
 public class Main {
 
     public static void main(String[] args) {
-//        MathEquation testEquation = new MathEquation();
-//        testEquation.execute();
-//        System.out.print("test=");
-//        System.out.println(testEquation.getResult());
+//        useMathEquation():
+//        useCalculatorBase();
 
+        String[] statements = {
+                "divide 100.0 50.0", //100.0 / 50.0 = 2.0
+                "add 25.0 92.0", //25.0 + 92.0 = 117.0
+                "subtract 225.0 17.0", //225.0 - 17.0 = 108.0
+                "multiply 11.0 3.0", //11.0 * 3.0 = 33.0
+
+        };
+
+        CalculateHelper helper = new CalculateHelper();
+        for (String statement:statements) {
+            helper.process(statement);
+            System.out.println(helper);
+        }
+
+    }
+    static void  useMathEquation() {
         MathEquation[] equations = new MathEquation[4];
-        //equations[0] = new MathEquation();
-
         equations[0] = new MathEquation('d',100.0d, 50.0d);
         equations[1] = new MathEquation('a',25.0d, 92.0d);
         equations[2] = new MathEquation('s',225.0d, 17.0d);
---
+++ src/com/synowa/calculator/MathCommand.java
@@ -0,0 +1,8 @@
+package com.synowa.calculator;
+
+public enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
